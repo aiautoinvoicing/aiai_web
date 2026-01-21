e8f02960a9b44caa22841bd27d46f62eb753b519
aiaccfin <aiaccfin@gmail.com>
2026-01-20 19:22:24 -0500

    v

--- app/ui/wages/table_main.tsx
+++ app/ui/wages/table_main.tsx
@@ -6,17 +6,11 @@ import { columns } from "./columns_wage";
 
 export default async function WagesTable({
     currentPage,
-    sortBy,
-    sortOrder,
-    searchParams,
 }: {
     currentPage: number;
-    sortBy?: string;
-    sortOrder?: "asc" | "desc";
-    searchParams?: Record<string, string | undefined>;
 }) {
 
-    const { items, total_pages } = await fetchWages({
+    const { items } = await fetchWages({
         page: currentPage,
     });
 
