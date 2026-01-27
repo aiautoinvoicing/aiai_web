71b788c1ee920246032fc484eef7d62242c55b2a
aiaccfin <aiaccfin@gmail.com>
2026-01-27 06:20:31 -0500

    update

--- app/config/backend.ts
+++ app/config/backend.ts
@@ -1,4 +1,6 @@
 // config/backend.ts
 
 export const BASE_URL = "http://34.130.233.222:8008";
-// export const BASE_URL = "http://local     host:8008";
+// export const BASE_URL = "http://localhost:8008";
+export const BACKEND_URL="https://vfastapi.vercel.app";
+export const HAYSTACKED_URL="https://haystacked.vercel.app";
